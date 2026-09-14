ec96633b68553e1e9441cc3c61cbd12d46f1a937
julietteortiz <juliette.a.ortiz@gmail.com>
2026-09-14 19:43:36 -0400

    check

--- model_analysis.py
+++ model_analysis.py
@@ -48,10 +48,10 @@ def calculate_y(
     y = 0
     count = 1
     for coef in coefficients:
-        print("Y: " + str(y))
-        print("EQ: " + str(coef) + str(x) + "^" + str(count))
+       # print("Y: " + str(y))
+       # print("EQ: " + str(coef) + str(x) + "^" + str(count))
         y += coef * (x ** count)
-        print("After Y: " + str(y))
+      #  print("After Y: " + str(y))
         count += 1
     return y
 
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     input2 =  np.array([1, 2, 3])
     coeffs1 = [1,2]
     coeffs2 = [2,3,4]
-    print("t1: should return 21\n")
-    print("t1: " + str(calculate_y(input1,coeffs1)))
-    print("\nt1: should return array\n")
-    print("t2: " + str(calculate_y(input2, coeffs2)))
+    #print("t1: should return 21\n")
+    #print("t1: " + str(calculate_y(input1,coeffs1)))
+    #print("\nt1: should return array\n")
+    #print("t2: " + str(calculate_y(input2, coeffs2)))
