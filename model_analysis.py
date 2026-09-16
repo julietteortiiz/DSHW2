15819e61b79e101d240b319fee2b9944c122649e
julietteortiz <juliette.a.ortiz@gmail.com>
2026-09-15 21:14:35 -0400

    shi getting done

--- model_analysis.py
+++ model_analysis.py
@@ -81,9 +81,9 @@ def plot_scatter(
     color: str,
 ) -> None:
     """Add labeled scatter data to axes."""
-    axes.scatter(x_values, y_values)
-    axes.set_title(label)
-    axes.tick_params(labelcolor=color)
+    axes.scatter(x_values, y_values, label = label, color = color)
+    axes.set_xlabel("Year")
+    axes.set_ylabel("Sea Ice Extent (mil of km^2)")
 
     
 
@@ -99,12 +99,11 @@ def plot_polynomial(
     """Add a polynomial curve to axes over the requested x-range."""
     # TODO: np.linspace is useful here.
     x = np.linspace(x_start, x_end, 100)
-    y = list()
-    for i in x:
-        y.append(calculate_y(i, coefficients))
-    axes.plot(x,y)
-    axes.set_title(label)
-    axes.tick_params(labelcolor=color)
+    y = calculate_y(x, coefficients)
+    axes.plot(x,y, label = label, color=color)
+    axes.set_xlabel("Year")
+    axes.set_ylabel("Sea Ice Extent (mil of km^2)")
+    
     
 
 def plot_elbow(axes: Axes, rss_values: Sequence[Number]) -> None:
@@ -124,18 +123,17 @@ def save_figure(figure: plt.Figure, filename: str) -> None:
 def main() -> None:
     """Run all required analyses and create the figures listed in the handout."""
     #Read In Data, convert to correct type
-    data2013 = read_in_data("data/sea_ice_2013-2020.csv", x_type=["int", "float"])
-    print(data2013)
+    data1979 = read_in_data("data/sea_ice_1979-2012.csv", x_type=["int", "float"])
     
 
     #split into x and y values
-    x_values = data2013[0]
-    y_values = data2013[1]
+    x_values = data1979[0]
+    y_values = data1979[1]
     fig1, axs = plt.subplots()
     plot_scatter(axs, x_values, y_values, label = "Initial Data", color = "Blue")
-    axs.set_xlabel("Year")
-    axs.set_ylabel("Sea Ice %")
+    axs.set_title("1979 Original Data")
     save_figure(fig1, "part1.pdf")
+
     
 
     #Calculate Y
@@ -143,18 +141,28 @@ def main() -> None:
     deg1 = calculate_y(x_np, DEGREE_1_COEFFICIENTS)
     fig2, axs = plt.subplots()
     plot_polynomial(axs, x_values[0], x_values[len(x_values)-1], DEGREE_1_COEFFICIENTS, label = "Degree 1 Polynomial", color = "Blue")
-    save_figure(fig2, "part2.pdf")
+    axs.set_title("1979 Polynomial 1st Degree")
+    save_figure(fig2, "part2_deg1.pdf")
 
     deg2 = calculate_y(x_np, DEGREE_2_COEFFICIENTS)
     fig3, axs = plt.subplots()
     plot_polynomial(axs, x_values[0], x_values[len(x_values)-1], DEGREE_2_COEFFICIENTS, label = "Degree 2 Polynomial", color = "Blue")
-    save_figure(fig3, "part3.pdf")
+    axs.set_title("1979 Polynomial 2nd Degree")
+    save_figure(fig3, "part2_deg2.pdf")
 
-    #Calculate Regressions
-    calculate_residuals(x_values, y_values, DEGREE_1_COEFFICIENTS)
-    calculate_residuals(x_values, y_values, DEGREE_2_COEFFICIENTS)
+    #Calculate Regressions + Plot Them
+    r1 = calculate_residuals(x_values, y_values, DEGREE_1_COEFFICIENTS)
+    r2 = calculate_residuals(x_values, y_values, DEGREE_2_COEFFICIENTS)
 
-
+    fig4, axs = plt.subplots()
+    axs.set_title("1979 Polynomial 1st Degree Residuals")
+    plot_scatter(axs, x_values, r1, label="Residuals for Degree1", color = "Blue")
+    save_figure(fig4, "part2_residuals1.pdf")
+    
+    fig5, axs = plt.subplots()
+    axs.set_title("1979 Polynomial 2nd Degree Residuals")
+    plot_scatter(axs, x_values, r2, label="Residuals for Degree2", color = "Blue")
+    save_figure(fig4, "part2_residuals2.pdf")
 
 if __name__ == "__main__":
     main()
